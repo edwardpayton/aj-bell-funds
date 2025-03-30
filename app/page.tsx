0904a42f9b867cf81223c5b02df1383649851950
Ed Payton <ed.payton@residently.com>
2025-03-30 11:56:49 +0100

    feat: css & adding mui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import { InvestmentData } from './components/InvestmentData';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import { FundManager } from './components/FundManager';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <InvestmentData />
-      </main>
-    </div>
+    <Container className={styles.page}>
+      <Box component="main" className={styles.main}>
+        <Typography variant="h1">Fund Selection</Typography>
+
+        <FundManager />
+      </Box>
+    </Container>
   );
 }
